62664a46519352a1d17d108c9830c92e08b66c79
Veljko Popovic <v.s.popovic@gmail.com>
2016-06-09 18:26:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+function createDeferFifo(doublelinkedlistbase, inherit, q) {
+  'use strict';
+
+  var ListItemCtor = doublelinkedlistbase.Item,
+    ListMixin = doublelinkedlistbase.Mixin;
+
+  function DeferFifoItem(content) {
+    ListItemCtor.call(this, content);
+  }
+  inherit(DeferFifoItem, ListItemCtor);
+  DeferFifoItem.prototype.apply = function(item) {
+    switch(item.action) {
+      case 'r':
+        this.content.resolve(item.value);
+        break;
+      case 'n':
+        this.content.notify(item.progress);
+        break;
+      case 'e':
+        this.content.reject(item.error);
+        break;
+      default:
+        throw Error('INVALID ITEM: '+JSON.stringify(item));
+    }
+    return func(this.content);
+  };
+
+  function DeferFifo(){
+    ListMixin.call(this);
+  }
+  ListMixin.addMethods(DeferFifo);
+  DeferFifo.prototype.destroy = function () {
+    this.reject(Error('ABORTED'));
+    ListMixin.prototype.destroy.call(this);
+  };
+  DeferFifo.prototype.defer = function(){
+    var d = q.defer(), newItem = new DeferFifoItem(d);
+    this.assureForController();
+    this.controller.addToBack(newItem);
+    return d.promise;
+  };
+  DeferFifo.prototype.resolve = function (result) {
+    this.assureForController();
+    this.controller.drain({action:'r', value: result});
+  };
+  DeferFifo.prototype.reject = function (error) {
+    this.assureForController();
+    this.controller.drain({action:'e', error: error});
+  };
+  DeferFifo.prototype.notify = function (progress) {
+    this.assureForController();
+    this.controller.traverse({action:'n', progress: progress});
+  };
+
+  return DeferFifo;
+}
+
+module.exports = createDeferFifo;
